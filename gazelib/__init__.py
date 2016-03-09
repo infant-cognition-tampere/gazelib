6fafa7005d128c3b3f0400a0e0adad513be0145d
xeli <akseli.palen@gmail.com>
2016-03-09 16:22:37 +0200

    Have package.py for easy package info reuse

--- gazelib/__init__.py
+++ gazelib/__init__.py
@@ -2,7 +2,4 @@
 '''
 Gazelib, a toolkit for gaze analysis.
 '''
-from gazelib import containers
-from gazelib import validation
-from gazelib import io
-from gazelib.version import version
+from gazelib.package import version as __version__
--- setup.py
+++ setup.py
@@ -13,92 +13,47 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-# Path to here to find README
-here = path.abspath(path.dirname(__file__))
+def get_package_info(module_name):
+    '''
+    Read info about package version, author etc from <module>/package.py
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+    Return
+        dict {'version': '1.2.3', 'author': 'John Doe', ...}
+    '''
+    here = path.dirname(path.abspath(__file__))
+    code_globals = {}
+    code_locals = {}
+    with open(path.join(here, module_name, 'package.py')) as f:
+        code = compile(f.read(), "package.py", 'exec')
+        exec(code, code_globals, code_locals)
+    return code_locals
 
-setup(
-    name='gazelib',
-    version='1.1.0',
-    description='Software tools to manage and analyze gaze data from eye-trackers',
-    long_description=long_description,
-    url='https://github.com/infant-cognition-tampere/gazelib',
-    author='Akseli Palen',
-    author_email='akseli.palen@gmail.com',
-    license='GPLv3',
+def get_long_description():
+    '''
+    Get the long description from the README file
+    '''
+    # Path to here to find README
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Scientific/Engineering :: Image Recognition',
-        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
-        'Topic :: Scientific/Engineering :: Information Analysis',
+# Get the version from gazelib/version.py
+# Modified from http://stackoverflow.com/a/21784019/638546
+def get_version(module_name, filename):
+    import re
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    here = path.dirname(path.abspath(__file__))
+    with open(path.join(here, module_name, filename)) as f:
+        version_file = f.read()
+        version_match = re.search(r'^__version__ = [\'\"]([^\'\"]*)[\'\"]',
+                                  version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
-
-    # What does your project relate to?
-    keywords='eye-tracking data',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=['gazelib'],
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    # Note for Tox: if install_requires changes, .tox must be recreated.
-    install_requires=['deepdiff', 'jsonschema'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['Sphinx']
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={},
-
-    # To use nose2 to run your package’s tests, add the following
-    tests_require=['nose2', 'unittest2'],
-    test_suite='nose2.collector.collector',
-)
+# Read setup parameters from package.py
+pkg_info = get_package_info('gazelib')
+pkg_info['long_description'] = get_long_description()
+setup(**pkg_info)
