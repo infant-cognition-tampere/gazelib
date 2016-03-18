3468820c9af8fc49f5c0f2d9caa75ea1d4ed599c
xeli <akseli.palen@gmail.com>
2016-03-18 15:23:38 +0200

    Fix issue #9

--- gazelib/io.py
+++ gazelib/io.py
@@ -8,7 +8,9 @@ import csv
 
 
 def load_json(filename):
-    # load json-type file
+    '''
+    Load json-type file and return its contents as python object.
+    '''
 
     with open(filename, 'r') as data_file:
         data = json.load(data_file)
@@ -16,21 +18,28 @@ def load_json(filename):
 
 
 def write_json(filename, data):
-    # Dump all the JSON in a file.
+    '''
+    Dump data to a given JSON file. File is created if does not exist.
+    '''
     with open(filename, 'w') as jsonfile:
         json.dump(data, jsonfile)
 
 
 def write_fancy_json(filename, data):
-    # Write json so that it is readable by humans (rowchange, indent..).
+    '''
+    Write JSON so that it is readable by humans (new lines, indent..).
+    '''
     with open(filename, 'w') as outfile:
         json.dump(data, outfile, sort_keys=True,
                   indent=4, ensure_ascii=False)
 
 
 def load_csv_as_dictlist(filename, delimit='\t', silent=True):
-    # Load a file in csv (common in .gazedata) and return data in JSON suitable
-    # for analysis.
+    '''
+    Load a file in csv (common in .gazedata) and return data as
+    list of dicts. Each dict contains the column names as keys and
+    cell values as dict values.
+    '''
 
     if not silent:
         print("Loading csv-file " + filename)
@@ -59,7 +68,6 @@ def load_csv_as_dictlist(filename, delimit='\t', silent=True):
 
     ifile.close()
 
-
     if not silent:
         print("File loaded succesfully. " + str(len(rows)) + " rows read.")
 
@@ -67,6 +75,11 @@ def load_csv_as_dictlist(filename, delimit='\t', silent=True):
 
 
 def write_dictlist_as_csv(target_filename, dictlist, delimit='\t'):
+    '''
+    Write given list of dicts to a file as CSV. Each dict is required
+    to have key for each column name. The column names are read from the
+    first dict.
+    '''
     keys = dictlist[0].keys()
     with open(target_filename, 'w') as csvfile:
         fieldnames = keys
