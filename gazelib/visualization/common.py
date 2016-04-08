1cb3b5c18cae5f89a884e57cc416991e531e1fa1
xeli <akseli.palen@gmail.com>
2016-04-08 16:08:45 +0300

    Implement render_overview without events

--- gazelib/visualization/common.py
+++ gazelib/visualization/common.py
@@ -13,7 +13,12 @@ def render_path(common, output_html_filepath, title='Path'):
     rys = common.get_stream_values('gazelib/gaze/right_eye_y_relative')
 
     p = plotting.figure(title=title, x_axis_label='X', y_axis_label='Y',
-                        plot_width=640, plot_height=480)
+                        plot_width=640, plot_height=480,
+                        x_range=(-0.2, 1.2), y_range=(-0.2, 1.2))
+
+    # Render screen rectangle
+    p.quad(left=[0.0], right=[1.0], top=[0.0], bottom=[1.0],
+           line_width=1, line_color='black', fill_alpha=0)
 
     # Visualize only valid points.
     # We do that by visualizing valid subpaths
@@ -39,6 +44,15 @@ def render_path(common, output_html_filepath, title='Path'):
         p.line(valid_xs, valid_ys, line_width=1, line_color='red')
         # Red as Right
 
+    # Display start points.
+    lfirst = lpaths[0][0]
+    rfirst = rpaths[0][0]
+    # Width and height in x_axis and y_axis units.
+    p.oval(x=lfirst[0], y=lfirst[1], width=0.17, height=0.2,
+           line_color='blue', fill_alpha=0)
+    p.oval(x=rfirst[0], y=rfirst[1], width=0.17, height=0.2,
+           line_color='red', fill_alpha=0)
+
     plotting.output_file(output_html_filepath, title)
     plotting.save(p)
 
@@ -47,5 +61,40 @@ def render_path_for_each_event(common, event_tag, output_html_filepath):
     pass
 
 
-def render_overview(common, output_html_filepath):
-    pass
+def render_overview(common, output_html_filepath, title='Overview'):
+    '''
+    Create HTML-based visualization from all streams and events.
+    Does not understand stream or event semantics like gaze paths for example.
+    '''
+
+    # Collect figures together
+    figs = []
+
+    def to_ms(micros):
+        return int(round(micros / 1000))
+
+    # Streams
+    for stream_name in common.get_stream_names():
+        tl_name = common.get_stream_timeline_name(stream_name)
+        x = list(map(to_ms, common.get_timeline(tl_name)))
+        y = common.get_stream_values(stream_name)
+
+        # Get valid substreams
+        sl = utils.get_valid_sublists_2d(x, y)
+
+        fig = plotting.figure(title=stream_name, x_axis_label='time (ms)',
+                              plot_width=1000, plot_height=300,
+                              toolbar_location=None)
+
+        for xs, ys in sl:
+            fig.line(xs, ys, line_color='black')
+
+        figs.append(fig)
+
+    # Events
+
+    # Lay out multiple figures
+    p = plotting.vplot(*figs)
+    # Save
+    plotting.output_file(output_html_filepath, title)
+    plotting.save(p)
--- gazelib/visualization/utils.py
+++ gazelib/visualization/utils.py
@@ -4,6 +4,9 @@
 def isNotNone(x):
     return x is not None
 
+def isNotNone2d(x, y):
+    return x is not None and y is not None
+
 
 def get_valid_sublists(l, validator=isNotNone):
     '''
@@ -16,6 +19,10 @@ def get_valid_sublists(l, validator=isNotNone):
         validator:
             A boolean function that returns true if element is valid.
             Defaults to testing if value is not None.
+
+    Example:
+        >>> get_valid_sublists([1, 2, None, 3])
+        [[1, 2], [3]]
     '''
     sublists = []
     cur_sublist = []
@@ -32,5 +39,37 @@ def get_valid_sublists(l, validator=isNotNone):
     # If last value was valid, one sublist remains to be added.
     if len(cur_sublist) > 0:
         sublists.append(cur_sublist)
+
+    return sublists
+
+def get_valid_sublists_2d(xs, ys, validator=isNotNone2d):
+    '''
+    Return list of lists where
+
+    Example result:
+      [
+        ([1,2,3,...], [0,3,5,...]),
+        ([4,6,2,...], [3,0,3,...]),
+        ...
+      ]
+    '''
+    sublists = []
+    cur_sublist_x = []
+    cur_sublist_y = []
+
+    for x, y in zip(xs, ys):
+        if validator(x, y):
+            cur_sublist_x.append(x)
+            cur_sublist_y.append(y)
+        else:
+            if len(cur_sublist_x) > 0:
+                # End current sublist and start a new one
+                sublists.append((cur_sublist_x, cur_sublist_y))
+                cur_sublist_x = []
+                cur_sublist_y = []
+
+    # If last value was valid, one sublist remains to be added.
+    if len(cur_sublist_x) > 0:
+        sublists.append((cur_sublist_x, cur_sublist_y))
 
     return sublists
