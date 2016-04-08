926ee4c96ed062d1e7295d98e8a4f1d0da210d15
xeli <akseli.palen@gmail.com>
2016-04-08 12:50:56 +0300

    Left-right path vis

--- gazelib/visualization/common.py
+++ gazelib/visualization/common.py
@@ -4,11 +4,16 @@ from . import utils
 
 
 def render_path(common, output_html_filepath, title='Path'):
-    xs = common.get_stream_values('gazelib/gaze/left_eye_x_relative')
-    ys = common.get_stream_values('gazelib/gaze/left_eye_y_relative')
+    '''
+    Create a HTML-based visualization of the gaze path.
+    '''
+    lxs = common.get_stream_values('gazelib/gaze/left_eye_x_relative')
+    lys = common.get_stream_values('gazelib/gaze/left_eye_y_relative')
+    rxs = common.get_stream_values('gazelib/gaze/right_eye_x_relative')
+    rys = common.get_stream_values('gazelib/gaze/right_eye_y_relative')
 
     p = plotting.figure(title=title, x_axis_label='X', y_axis_label='Y',
-                        plot_width=400, plot_height=300)
+                        plot_width=640, plot_height=480)
 
     # Visualize only valid points.
     # We do that by visualizing valid subpaths
@@ -16,14 +21,23 @@ def render_path(common, output_html_filepath, title='Path'):
     def validator(x):
         return x[0] is not None and x[1] is not None
 
-    paths = utils.get_valid_sublists(zip(xs, ys), validator)
+    lpaths = utils.get_valid_sublists(zip(lxs, lys), validator)
+    rpaths = utils.get_valid_sublists(zip(rxs, rys), validator)
 
-    for valid_path in paths:
+    for valid_path in lpaths:
         valid_xs = list(map(lambda x: x[0], valid_path))
         valid_ys = list(map(lambda x: x[1], valid_path))
 
-        p.cross(valid_xs, valid_ys, size=10)
-        p.line(valid_xs, valid_ys, line_width=1)
+        p.cross(valid_xs, valid_ys, size=10, line_color='blue')
+        p.line(valid_xs, valid_ys, line_width=1, line_color='blue')
+
+    for valid_path in rpaths:
+        valid_xs = list(map(lambda x: x[0], valid_path))
+        valid_ys = list(map(lambda x: x[1], valid_path))
+
+        p.cross(valid_xs, valid_ys, size=10, line_color='red')
+        p.line(valid_xs, valid_ys, line_width=1, line_color='red')
+        # Red as Right
 
     plotting.output_file(output_html_filepath, title)
     plotting.save(p)
