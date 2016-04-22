46753a149832282e2200d9512321fd9e5d0c0b5d
xeli <akseli.palen@gmail.com>
2016-04-22 14:34:33 +0300

    Implement fill_gaps

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,26 @@
+try:
+    import unittest2 as unittest  # to support Python 2.6
+except ImportError:
+    import unittest
+
+from gazelib import preprocessing as unit
+
+
+class TestPreprocessing(unittest.TestCase):
+
+    def test_fill_gaps(self):
+
+        a = [1, None, 2]
+        b = [None, 1, 2]
+        c = [None, None, None]
+        fa = unit.fill_gaps(a)
+        self.assertEqual(fa, [1, 1, 2])
+        fb = unit.fill_gaps(b)
+        self.assertEqual(fb, [1, 1, 2])
+
+        f = lambda: unit.fill_gaps(c)
+        self.assertRaises(unit.ExtrapolationError, f)
+
+
+if __name__ == '__main__':
+    unittest.main()
